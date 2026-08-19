1cea450f12f79d11da6a6261f6e0786b11566156
Anhpika <anhpika12345@gmail.com>
2026-08-19 16:36:15 +0700

    Bài 3: Post Detail với Comments

--- src/pages/PostDetail/index.jsx
+++ src/pages/PostDetail/index.jsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Loading from "../../components/Loading";
+
+import './PostDetail.module.scss'
 function PostDetail() {
-    return <h1>PostDetail page</h1>;
+    const params = useParams();
+    const navigate = useNavigate();
+
+    const [post, setPost] = useState(null);
+    const [comments, setComments] = useState(null);
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+            .then((res) => {
+                if (!res.ok) {
+                    navigate("/posts", { replace: true });
+                    return;
+                }
+                return res.json();
+            })
+            .then((data) => setPost(data));
+    }, [params.id, navigate]);
+
+    useEffect(() => {
+        fetch(
+            `https://jsonplaceholder.typicode.com/posts/${params.id}/comments`,
+        )
+            .then((res) => res.json())
+            .then((data) => setComments(data));
+    }, [params.id]);
+
+    if (!post) return <Loading />;
+
+    return (
+        <>
+            <button onClick={() => navigate(-1)}>Back</button>
+            <h1 style={{ fontWeight: "bold" }}>
+                {post.id}. {post.title}
+            </h1>
+            <p style={{ fontStyle: "italic" }}>UserId: {post.userId}</p>
+            <p>Body: {post.body}</p>
+
+            <h2>Comment: </h2>
+            {comments?.map((comment) => (
+                <p key={comment.id}>
+                    <strong>Name:</strong> {comment.name} <br/>
+                    <strong>Email:</strong> {comment.email} <br/>
+                    <strong>Body:</strong> {comment.body} <br/>
+                    </p>
+            ))}
+        </>
+    );
 }
 
 export default PostDetail;
